e686892bbcbb1b6ac41f48f27440f3636033c147
Michael Iaria <8315738+mpiaria@users.noreply.github.com>
2025-01-11 18:43:57 -0600

    Update nunjucks.test.ts

--- test/nunjucks/nunjucks.test.ts
+++ test/nunjucks/nunjucks.test.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import { describe, it } from "@jest/globals";
 const nunjucks = require("nunjucks");
 
-describe("Nunjucks Test Suite", () => {
+describe("Nunjucks Utility", () => {
 	const context = {
 		author: "Michael Iaria",
 		description: "A GitHub template repository used to create Node.js modules written in TypeScript",
@@ -15,7 +15,7 @@ describe("Nunjucks Test Suite", () => {
 
 	// You can delete this directory (test/nunjucks) after you have generated an appropriate package.json.
 	// You can also uninstall nunjucks if you don't need it elsewhere:  `npm uninstall nunjucks`.
-	it.skip("Generate package.json with specified values", () => {
+	it.skip("Render package.json", () => {
 		const rendered = nunjucks.render(path.join(__dirname, "package.json.njk"), context);
 		fs.writeFileSync(path.join(__dirname, "..", "..", "package.json"), rendered);
 	});
